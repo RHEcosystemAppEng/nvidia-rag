dadc9945c3326b0ce767575fe4b442af26d20fb8
sauagarwa <sauagarw@redhat.com>
2025-03-17 17:49:41 -0400

    NVIDIA RAG Blueprint using OpenShift AI

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+## Changelog
+All notable changes to this project will be documented in this file.
+The format is based on Keep a Changelog, and this project adheres to Semantic Versioning.
+
+## [1.0.0] - 2025-01-15
+
+### Added
+
+- First release.
+
---
+++ deploy/helm/deploy.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+read -r -p "Enter NGC API Key: " NVIDIA_API_KEY
+echo "$NVIDIA_API_KEY"
+
+kubectl create namespace rag
+kubectl create secret -n rag docker-registry ngc-secret --docker-server=nvcr.io --docker-username='$oauthtoken' --docker-password=$NVIDIA_API_KEY
+kubectl label secret ngc-secret -n rag app.kubernetes.io/managed-by=Helm
+kubectl annotate secret ngc-secret -n rag meta.helm.sh/release-name=rag meta.helm.sh/release-namespace=rag
+
+kubectl create secret -n rag generic nvidia-nim-secrets --from-literal=NGC_API_KEY=$NVIDIA_API_KEY
+kubectl label secret nvidia-nim-secrets -n rag app.kubernetes.io/managed-by=Helm
+kubectl annotate secret nvidia-nim-secrets -n rag meta.helm.sh/release-name=rag meta.helm.sh/release-namespace=rag
+
+helm upgrade --install rag . -n rag \
+  --set imagePullSecret.password=$NVIDIA_API_KEY \
+  --set nvidia-nim-llama-32-nv-embedqa-1b-v2.nim.ngcAPIKey=$NVIDIA_API_KEY \
+  --set text-reranking-nim.nim.ngcAPIKey=$NVIDIA_API_KEY \
+  --set nim-llm.model.ngcAPIKey=$NVIDIA_API_KEY \
+  --set text-reranking-nim.acceleratorName=g5-gpu \
+  --set-json text-reranking-nim.tolerations='[{"key":"g5-gpu","effect":"NoSchedule","operator":"Exists"}]' \
+  --set nvidia-nim-llama-32-nv-embedqa-1b-v2.acceleratorName=g5-gpu \
+  --set-json nvidia-nim-llama-32-nv-embedqa-1b-v2.tolerations='[{"key":"g5-gpu","effect":"NoSchedule","operator":"Exists"}]' \
+  --set nim-llm.acceleratorName=g5-gpu \
+  --set-json nim-llm.tolerations='[{"key":"g5-gpu","effect":"NoSchedule","operator":"Exists"}]' \
+  --set-json milvus.tolerations='[{"key":"g5-gpu","effect":"NoSchedule","operator":"Exists"}]'
+
+oc adm policy add-scc-to-user anyuid -z rag-minio -n rag
+
+echo "Listing pods..."
+kubectl get pods -n rag
+
+echo "Listing services..."
+kubectl get svc -n rag
